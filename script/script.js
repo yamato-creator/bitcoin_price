5d5026fab98aa68a6b1a99c198ae5ed7278b6609
yamato-creator <dydydydydydy5656@gmail.com>
2021-07-12 15:34:28 +0900

    add

--- script/script.js
+++ script/script.js
@@ -2,5 +2,17 @@ var app = new Vue({
   el: '#app',
   data: {
     bpi: null
+  },mounted: function() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+      //console.log(response.data.bpi)
+      //console.log(response.data.bpi.USD.rate_float)
+      this.bpi = response.data.bpi
+      // WindowオブジェクトではなくVueオブジェクトを指定するため.bind(this)を追加
+    }.bind(this))
+    //エラー表示
+    .catch(function(error){
+      console.log(error)
+    })
   }
 })
