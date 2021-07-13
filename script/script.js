37ffbdb6e22b64b07ea608ea099a0b96a3da61db
yamato-creator <dydydydydydy5656@gmail.com>
2021-07-13 13:03:17 +0900

    add

--- script/script.js
+++ script/script.js
@@ -17,6 +17,7 @@ var app = new Vue({
       console.log(error)
       this.hasError = true
     }.bind(this))
+    // 通信に関する全ての処理が終わった時に呼び出される
     .finally(function(){
       this.loading = false
     }.bind(this))
